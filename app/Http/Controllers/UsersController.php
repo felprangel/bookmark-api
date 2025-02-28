b9ec9854d5db63063be7e500c50a8cfe909399b8
felprangel <feliperangel897@gmail.com>
2025-02-27 22:09:28 -0300

    adiciona lógica para registro de usuário

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
 class UsersController
 {
     public function createUser()
     {
-        return 'hello world';
+        $data = Request::validate([
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', Rule::unique('users')],
+            'password' => ['required', 'confirmed', Password::min(8)]
+        ]);
+
+        $user = User::create($data);
+        Auth::login($user);
     }
 
     public function login()
