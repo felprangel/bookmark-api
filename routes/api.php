b6eb44ce9dda17ff2dee28618ef26be65c1434fa
felprangel <feliperangel897@gmail.com>
2025-02-27 21:48:53 -0300

    adiciona rota de login

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\UsersController;
 use Illuminate\Routing\Route;
 
 Route::post('/register', [UsersController::class, 'createUser']);
+Route::post('/login', [UsersController::class, 'login']);
