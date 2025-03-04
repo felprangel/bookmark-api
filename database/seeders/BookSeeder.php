08c79e72cd38c98d5d08e8031704212f86f02bd1
felprangel <feliperangel897@gmail.com>
2025-03-04 16:01:55 -0300

    adiciona seeder de livros

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Str;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('books')->insert([
+            'user_id' => 1,
+            'title' => Str::random(20),
+            'author' => Str::random(20),
+            'pages' => random_int(0, 500),
+            'read' => random_int(0, 1)
+        ]);
+    }
+}
