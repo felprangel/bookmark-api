73c84d39ea85d22d2a290b60dd5769c903b193ed
felprangel <feliperangel897@gmail.com>
2025-03-04 15:51:18 -0300

    move a rota para o grupo de middleware

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UsersController::class, 'createUser']);
 Route::post('/login', [UsersController::class, 'login']);
-Route::post('/logout', [UsersController::class, 'logout']);
 
 Route::middleware(Authenticate::class . ':sanctum')->group(function () {
+    Route::post('/logout', [UsersController::class, 'logout']);
+
     Route::get('/books', [BooksController::class, 'getBooksByUser']);
     Route::post('/book', [BooksController::class, 'registerBook']);
     Route::patch('/book/read', [BooksController::class, 'readBook']);
