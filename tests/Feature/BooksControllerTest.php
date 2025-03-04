1d76e58e3d2d914b0525a91ba88d39d7b098124d
felprangel <feliperangel897@gmail.com>
2025-03-04 20:58:04 -0300

    adiciona teste para o controller de livros

---
+++ tests/Feature/BooksControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\User;
+use Database\Seeders\BookSeeder;
+use Laravel\Sanctum\Sanctum;
+
+it('should return all books of the logger user', function () {
+    Sanctum::actingAs(
+        User::factory()->create()
+    );
+    $this->seed(BookSeeder::class);
+
+    $response = $this->get('/books');
+
+    $response->assertOk();
+    $response->assertJson();
+});
