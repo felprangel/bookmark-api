06630f229521f51b8d096a363bae83627e154749
felprangel <feliperangel897@gmail.com>
2025-03-04 21:20:00 -0300

    adiciona função do controller para buscar os dados do usuário

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Book;
+use Illuminate\Support\Facades\Request;
 
 class BooksController
 {
-    //
+    public function getUserBooks()
+    {
+        $data = Request::validate([
+            'page' => ['required', 'integer']
+        ]);
+
+        $books = Book::getBooksByUser($data['page']);
+        return $books;
+    }
 }
