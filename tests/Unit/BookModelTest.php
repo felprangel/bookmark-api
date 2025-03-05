b9089e2e7cbf3b4c48cd22ad6549b22d7223b6d4
felprangel <feliperangel897@gmail.com>
2025-03-04 21:39:26 -0300

    ajusta teste do model

--- tests/Unit/BookModelTest.php
+++ tests/Unit/BookModelTest.php
@@ -1,9 +1,21 @@
 <?php
 
+use App\Models\Book;
 use App\Models\User;
 use Database\Seeders\BookSeeder;
+use Illuminate\Support\Facades\Auth;
 
 it('should return the correct books by user', function () {
-    User::factory()->create();
+    $user = User::factory()->create();
     $this->seed(BookSeeder::class);
+
+    Auth::login($user);
+
+    $books = Book::getBooksByUser(1);
+
+    expect($books)->toHaveCount(10);
+
+    $books = Book::getBooksByUser(2);
+
+    expect($books)->toHaveCount(5);
 });
