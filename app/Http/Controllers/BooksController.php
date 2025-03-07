584d1b22785438013bfe038b45f349443613db10
felprangel <feliperangel897@gmail.com>
2025-03-07 20:15:59 -0300

    adiciona função para criar livro no controller

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
 class BooksController
@@ -15,5 +16,19 @@ class BooksController
 
         $books = Book::getBooksByUser($data['page']);
         return $books;
+    }
+
+    public function registerBook()
+    {
+        $data = Request::validate([
+            'title' => ['required', 'string'],
+            'author' => ['required', 'string'],
+            'pages' => ['required', 'integer'],
+            'read' => ['required', 'boolean']
+        ]);
+
+        $book = new Book($data);
+        $book->user_id = Auth::id();
+        $book->save;
     }
 }
